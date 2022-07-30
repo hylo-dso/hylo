37f5cd074e44b2e2a5c8fdc4c0c556daedaf8b16
hylo-dso <103466529+hylo-dso@users.noreply.github.com>
2022-07-30 00:07:40 -0400

    add densenet.

--- dso/utils/models.py
+++ dso/utils/models.py
@@ -1,9 +1,10 @@
-from ..models.cifar import resnet32, mobilenetv2
+from ..models.cifar import resnet32, mobilenetv2, densenet
 from torchvision.models import resnet50
 from .cnn_utils.unet import UNet
 
 def get_model(name, **kwargs):
     networks = {
+        'densenet': densenet,
         'mobilenetv2': mobilenetv2,
         'resnet32': resnet32,
         'resnet50': resnet50,
